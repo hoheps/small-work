b36e7702b2607af4bf04df82dfac5655a5ed9674
hoheps <38477462+hoheps@users.noreply.github.com>
2019-05-29 04:41:07 -0400

    basic model for winrate works

--- csgo_winrate.py
+++ csgo_winrate.py
@@ -1,13 +1,53 @@
 from bs4 import BeautifulSoup
 import re
+import collections
 
 with open('steam.html', 'r') as f:
     data = f.read()
 
 soup = BeautifulSoup(data,"lxml")
-matches = soup.tr.find_next_siblings()
 
-maptitle = soup.tr.find_next_siblings()[0].td.find_all()[4]
+owner_username = soup.find_all('a',limit=2)[-1].text
+
+matches = soup.tr.find_next_siblings()
+winlose = {"W":[],"L":[],"T":[]}
+for match in matches:
+    userinfo = match.td.find_next_sibling().find_all("td")
+    #not the prettiest
+    team1 = [re.sub("\n", "", userinfo[playerseed].text)[1:] for playerseed in range(0,40,8)]
+    team1score,_,team2score = userinfo[40].text.split()
+    team1score = int(team1score)
+    team2score = int(team2score)
+    team2 = [re.sub("\n", "", userinfo[playerseed].text)[1:] for playerseed in range(41,80,8)]
+    #basically takes the name from userinfo[0] and removes the newlines, and the first space. quick and dirty
+    if team1score == team2score:
+        print("tie")
+        if owner_username in team1:
+            winlose["L"].append(team1)
+        if owner_username in team2:
+            winlose["L"].append(team2)
+    #ties are basically losses... right
+    elif (owner_username in team1 and team1score > team2score) or (owner_username in team2 and team2score > team1score):
+        print("win")
+        if owner_username in team1:
+            winlose["W"].append(team1)
+        if owner_username in team2:
+            winlose["W"].append(team2)
+    else:
+        print("lose")
+        if owner_username in team1:
+            winlose["L"].append(team1)
+        if owner_username in team2:
+            winlose["L"].append(team2)
+lose = collections.Counter([item for list in winlose["L"] for item in list])
+win = collections.Counter([item for list in winlose["W"] for item in list])
+for x in set(win).intersection(lose):
+    print(x, win[x]/(win[x]+lose[x]), win[x]+lose[x])
+#tie = collections.Counter([item for list in winlose["T"] for item in list])
+
+#future data processing
+
+"""maptitle = soup.tr.find_next_siblings()[0].td.find_all()[4]
 date = soup.tr.find_next_siblings()[0].td.find_all()[6]
 waittime = soup.tr.find_next_siblings()[0].td.find_all()[8]
 matchlength = soup.tr.find_next_siblings()[0].td.find_all()[10]
@@ -24,7 +64,7 @@ d = userinfo[4].text
 #keep in mind the next 2 can be blank
 mvp = userinfo[5].text
 hsp = userinfo[6].text
-score = userinfo[7].text
+score = userinfo[7].text"""
 for playerseed in range(0,40,8):
     pass
 score = userinfo[40].text
@@ -41,25 +81,23 @@ for playerseed in range(41,80,8):
 #maybe I should make sql for this lmao
 class Match:
     def __init__(self, matchid):
-        self.matchid = 
+        self.matchid = matchid
     def __str__(self):
-        return matchtime
+        pass#return matchtime
     def add_player(self, Player, team):
-
+        pass
     def mapname(self):
-        return mapname
+        pass#return mapname
     def teammates(self):
-
+        pass
     def players(self):
-        return players
+        pass#return players
 
 class Player:
     def __init__(self, name, id):
         self.name
-        self.id = 
-
+        self.id
     def __str__(self):
         return self.name
-
     def __repr__(self):
         return "player({})".format(self.name)
