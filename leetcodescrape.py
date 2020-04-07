8e3024d77375c533d9f7890603e189aa1e683880
hoheps <38477462+hoheps@users.noreply.github.com>
2020-04-06 20:56:50 -0700

    modified leetcodescrape to work more efficiently

--- leetcodescrape.py
+++ leetcodescrape.py
@@ -13,7 +13,7 @@ def scrape():
     con = sqlite3.connect('leetcodeq.db')
     cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS question_list")
-    cur.execute("CREATE TABLE question_list (id KEY, title TEXT, content TEXT, link TEXT);")
+    cur.execute("CREATE TABLE question_list (id KEY, title TEXT, problem TEXT, content TEXT, link TEXT);")
     question_id = 0
     for txt in soup.select("body p a"):
         if not re.search(r'(?<=\").*/(?=\")', str(txt)):
@@ -22,12 +22,16 @@ def scrape():
         req = requests.get(link, headers=headers)
         insoup = BeautifulSoup(req.content, 'lxml')
         title = insoup.select("h1 a")[0].get_text()
-        for line in insoup.select("div.entrybody p"):
-            while line.span:
-                line.span.decompose()
-        content = "\n".join((x.get_text() for x in insoup.select("div.entrybody")))
-        content = re.search(r'[\s\S]*(?=Related posts)', content).group()
-        cur.execute("INSERT INTO question_list VALUES (?,?,?,?);", (question_id, title, content, link))
+        questionline = 0
+        for i,line in enumerate(insoup.select("div.entrybody p")):
+            while line.span: #finds the span tags, which contains ads
+                line.span.decompose() #removes them
+            if line.strong and not questionline: #gets the first strong tag, which usually means it's the answer/analysis
+                questionline = i
+        problem = "\n".join((x.get_text() for x in insoup.select("div.entrybody p")[:questionline]))
+        content = "\n".join((x.get_text() for x in insoup.select("div.entrybody p")[questionline:]))
+        #content = re.search(r'[\s\S]*(?=Related posts)', content).group() #removes relatedposts content, but related posts aren't in the p tag so commented out
+        cur.execute("INSERT INTO question_list VALUES (?,?,?,?,?);", (question_id, title, problem, content, link))
         question_id += 1
         con.commit()
     con.close()
@@ -41,6 +45,16 @@ def generate():
     return cur.fetchone()
 
 if __name__ == '__main__':
-    # scrape()
-    for x in generate():
-        print(x)
+    scrape()
+    #for x in generate():
+    #    print(x)
+
+# commands for export
+# sqlite> .mode list
+# sqlite> .separator $
+# sqlite> .output test_file_2.txt
+# sqlite> SELECT title,problem, content  FROM question_list;
+# sqlite> .exit
+# perl -pe 's/\n/<br>/g' -i test_file_2.txt
+# perl -pe 's/LeetCode – /\n/g' -i test_file_2.txt  
+#perl commands make it easier to import to some programs
